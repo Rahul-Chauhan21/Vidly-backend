7a78fcca1ae7d4ace8e63b25584baa5826895224
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-11-01 20:46:09 +0530

    Connected to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const genres = require("./routes/genres");
 const home = require("./routes/home");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB..");
+  })
+  .catch((err) => console.error("Could not connect to MongoDB..", err.message));
 
 app.use(express.json());
 app.use("/api/genres", genres);
