c0ff36f90e43a0a796983f9fdecdca1d91a01bbb
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-11-08 22:21:59 +0530

    Added genreSchema

--- models/genre.js
+++ models/genre.js
@@ -1,19 +1,19 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
-const Genre = mongoose.model(
-  "Genre",
-  new mongoose.Schema({
-    name: { type: String, required: true, minlength: 5, maxlength: 50 },
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 5, maxlength: 50 },
+});
+
+const Genre = mongoose.model("Genre", genreSchema);
 
 const validateGenre = (genre) => {
   const schema = Joi.object({
-    name: Joi.string().min(2).required(),
+    name: Joi.string().min(3).required(),
   });
   return schema.validate(genre);
 };
 
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validateGenre = validateGenre;
