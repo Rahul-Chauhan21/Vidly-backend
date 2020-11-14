68c4ca16f543a974e73002d2f8abe66f1de29c2f
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-11-14 23:41:44 +0530

    Registering Users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 const express = require("express");
+const users = require("./routes/users");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers.js");
 const home = require("./routes/home");
@@ -24,6 +25,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/rentals", rentals);
 app.use("/", home);
+app.use("/api/users", users);
 app.use("/api/movies", movies);
 
 const port = process.env.PORT || 3000;
