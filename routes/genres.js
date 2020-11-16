1b4022485772457c7fdd26a7786f1dda7770c09d
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-11-16 23:44:57 +0530

    Using role based Authorization

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 const { validateGenre, Genre } = require("../models/genre");
 const mongoose = require("mongoose");
 const express = require("express");
@@ -56,7 +57,7 @@ router.put("/:id", auth, async (req, res) => {
 });
 
 // delete an existing genre
-router.delete("/:id", auth, async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) {
