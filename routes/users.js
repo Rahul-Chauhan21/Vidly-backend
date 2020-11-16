7d32c6ee2f10b4de17ae41f684e95942c2ff11aa
Rahul-Chauhan21 <crahul1721@gmail.com>
2020-11-16 23:24:15 +0530

    Added route to get user info

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
+const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { validateUser, User } = require("../models/user");
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validateUser(req.body);
